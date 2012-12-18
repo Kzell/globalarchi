cd6acbe34895e65f69bdf4c82234d266210492f8
Thomas <thomas.cierco@gmail.com>
2012-12-18 21:58:56 +0100

    css sur le logo

--- wp-content/themes/globalarchi/home.php
+++ wp-content/themes/globalarchi/home.php
@@ -1,4 +1,6 @@
 <?php get_header(); ?>
+		<h1 id="logo"><a href="index.html" rel=""><img src="<?php bloginfo('template_directory');?>/images/design/logo.png" alt="" /></a></h1>
+
 	<img alt="Projet" class="bg_fullscreen" src="<?php bloginfo('template_directory');?>/images/visuals/background_home.jpg" />
 	<span class="fullscreen_pattern">&nbsp;</span>
 	<?php if ( have_posts() ) : ?>
