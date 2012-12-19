4ad869ce1eb58b6e54de360b693f33bd362d7a15
Thomas <thomas.cierco@gmail.com>
2012-12-19 12:53:23 +0100

    correction menu

--- wp-content/themes/globalarchi/home.php
+++ wp-content/themes/globalarchi/home.php
@@ -4,10 +4,12 @@
 		<div class="wrapper">
 		
 			<section id="logo">
-				<h1>
-					<img src="<?php bloginfo('template_directory');?>/images/design/logo.png" alt="" />
-					<span><?php bloginfo( 'description' ); ?></span>
-				</h1>
+				<div class="inner">
+					<h1>
+						<img src="<?php bloginfo('template_directory');?>/images/design/logo.png" alt="" />
+						<span><?php bloginfo( 'description' ); ?></span>
+					</h1>
+				</div>
 			</section>
 			<section id="references">
 
