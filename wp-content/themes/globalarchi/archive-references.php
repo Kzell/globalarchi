90b2a7a23bb4d7f960d9192607957f4006754cdf
Loick <loick.ledigabel@gmail.com>
2012-12-19 23:30:08 +0100

    ref

--- wp-content/themes/globalarchi/archive-references.php
+++ wp-content/themes/globalarchi/archive-references.php
@@ -31,6 +31,8 @@
 
 			  		$('.photos_list').empty();
 
+			  		console.log(response);
+
 			  		$.each(response, function(value, key) {
 			  			$li = $('<li>');
 			  			$a = $('<a>').attr('href',key[1]);
@@ -69,60 +71,56 @@
 				<img src="<?php bloginfo('template_directory');?>/images/design/logo.png" alt="" />
 				<span><?php bloginfo( 'description' ); ?></span>
 			</h1>
-		<div class="filters">
-		<form>
-		<?php
-				$query = new WP_Query( array( 'post_type' => 'architectes', 'posts_per_page' => -1));
-				$posts = $query->posts;
-				$arrayArchitectes = array();
-		
-				echo '<select name="architectes" id="order_architecte">';
-				echo '<option value="" selected>Tous</option>';
-		
-				foreach ($posts as $post){
-					$name = get_the_title();
-					if(!in_array($name, $arrayArchitectes)){
-						array_push($arrayArchitectes, $name);
-						?><option value="<?php the_title(); ?>"><?php the_title(); ?></option><?php
-					}
-				}
-				echo '</select>';
-		
-				$countries = $wpdb->get_results('
-					SELECT distinct meta_value
-					FROM '.$wpdb->posts.', '.$wpdb->postmeta.'
-					WHERE post_type="references"
-					AND meta_key = "country"
-					ORDER BY meta_value ASC'
-				);
-		
-				echo '<select name="country" id="order_country">';
-				echo '<option value="" selected>Tous</option>';
-		
-				foreach ($countries as $country){
-					?><option value="<?php echo $country->meta_value; ?>"><?php echo $country->meta_value; ?></option><?php
-				}
-				echo '</select>';
-			?>
-				<select name="date" id="order_date">
-					<option value="desc" selected>Décroissant</option>
-					<option value="asc">Croissant</option>
-				</select>
-		</form>
-		
-		
-			<img src="<?php bloginfo('template_directory') ?>/images/spinner.gif" alt="loader" class="loader"/>
-		</div><!--End filters-->
+			<div class="filters">
+				<form>
+				<?php
+						$query = new WP_Query( array( 'post_type' => 'architectes', 'posts_per_page' => -1));
+						$posts = $query->posts;
+						$arrayArchitectes = array();
+				
+						echo '<select name="architectes" id="order_architecte">';
+						echo '<option value="" selected>Tous</option>';
+				
+						foreach ($posts as $post){
+							$name = get_the_title();
+							if(!in_array($name, $arrayArchitectes)){
+								array_push($arrayArchitectes, $name);
+								?><option value="<?php the_title(); ?>"><?php the_title(); ?></option><?php
+							}
+						}
+						echo '</select>';
+				
+						$countries = $wpdb->get_results('
+							SELECT distinct meta_value
+							FROM '.$wpdb->posts.', '.$wpdb->postmeta.'
+							WHERE post_type="references"
+							AND meta_key = "country"
+							ORDER BY meta_value ASC'
+						);
+				
+						echo '<select name="country" id="order_country">';
+						echo '<option value="" selected>Tous</option>';
+				
+						foreach ($countries as $country){
+							?><option value="<?php echo $country->meta_value; ?>"><?php echo $country->meta_value; ?></option><?php
+						}
+						echo '</select>';
+					?>
+						<select name="date" id="order_date">
+							<option value="desc" selected>Décroissant</option>
+							<option value="asc">Croissant</option>
+						</select>
+				</form>
+				<img src="<?php bloginfo('template_directory') ?>/images/spinner.gif" alt="loader" class="loader"/>
+			</div><!--End filters-->
 		</div><!--End inner-->
 	</section>
-<ul id="results"></ul>
 
-
-<section id="main" class="box-wrap antiscroll-wrap">
-			<div id="content" class="box">
-				<ul class="photos_list antiscroll-inner"></ul>
-			</div><!--END #content-->
-		</section><!--END #main-->
+	<section id="main" class="box-wrap antiscroll-wrap">
+		<div id="content" class="box">
+			<ul class="photos_list antiscroll-inner"></ul>
+		</div><!--END #content-->
+	</section><!--END #main-->
 </div>
 
 <?php get_footer(); ?>
