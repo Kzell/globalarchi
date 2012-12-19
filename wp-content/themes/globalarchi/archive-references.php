403135d9011fb7516dbb20513a6200639ce0e9e2
Thomas <thomas.cierco@gmail.com>
2012-12-19 17:13:06 +0100

    modif taille wrapper + debut page references list

--- wp-content/themes/globalarchi/archive-references.php
+++ wp-content/themes/globalarchi/archive-references.php
@@ -49,56 +49,234 @@
 		}
 	}); 
 </script>
+<div id="content" class="references_list">
 
-<div id="wrapper">
+<div class="wrapper">
+	<section id="logo">
+		<div class="inner">
+			<h1>
+				<img src="<?php bloginfo('template_directory');?>/images/design/logo.png" alt="" />
+				<span><?php bloginfo( 'description' ); ?></span>
+			</h1>
+		<div class="filters">
+		<form>
+		<?php
+				$query = new WP_Query( array( 'post_type' => 'architectes', 'posts_per_page' => -1));
+				$posts = $query->posts;
+				$arrayArchitectes = array();
+		
+				echo '<select name="architectes" id="order_architecte">';
+				echo '<option value="" selected>Tous</option>';
+		
+				foreach ($posts as $post){
+					$name = get_the_title();
+					if(!in_array($name, $arrayArchitectes)){
+						array_push($arrayArchitectes, $name);
+						?><option value="<?php the_title(); ?>"><?php the_title(); ?></option><?php
+					}
+				}
+				echo '</select>';
+		
+				$countries = $wpdb->get_results('
+					SELECT distinct meta_value
+					FROM '.$wpdb->posts.', '.$wpdb->postmeta.'
+					WHERE post_type="references"
+					AND meta_key = "country"
+					ORDER BY meta_value ASC'
+				);
+		
+				echo '<select name="country" id="order_country">';
+				echo '<option value="" selected>Tous</option>';
+		
+				foreach ($countries as $country){
+					?><option value="<?php echo $country->meta_value; ?>"><?php echo $country->meta_value; ?></option><?php
+				}
+				echo '</select>';
+			?>
+		
+			<select name="date" id="order_date">
+				<option value="desc" selected>Décroissant</option>
+				<option value="asc">Croissant</option>
+			</select>
+		</form>
+		
+		
+			<img src="<?php bloginfo('template_directory') ?>/images/spinner.gif" alt="loader" class="loader"/>
+		</div><!--End filters-->
+		</div><!--End inner-->
+	</section>
 
-	<?php
-		$query = new WP_Query( array( 'post_type' => 'architectes', 'posts_per_page' => -1));
-		$posts = $query->posts;
-		$arrayArchitectes = array();
-
-		echo '<select name="architectes" id="order_architecte">';
-		echo '<option value="" selected>Tous</option>';
-
-		foreach ($posts as $post){
-			$name = get_the_title();
-			if(!in_array($name, $arrayArchitectes)){
-				array_push($arrayArchitectes, $name);
-				?><option value="<?php the_title(); ?>"><?php the_title(); ?></option><?php
-			}
-		}
-		echo '</select>';
-
-		$countries = $wpdb->get_results('
-			SELECT distinct meta_value
-			FROM '.$wpdb->posts.', '.$wpdb->postmeta.'
-			WHERE post_type="references"
-			AND meta_key = "country"
-			ORDER BY meta_value ASC'
-		);
-
-		echo '<select name="country" id="order_country">';
-		echo '<option value="" selected>Tous</option>';
-
-		foreach ($countries as $country){
-			?><option value="<?php echo $country->meta_value; ?>"><?php echo $country->meta_value; ?></option><?php
-		}
-		echo '</select>';
-	?>
-
-	<select name="date" id="order_date">
-		<option value="desc" selected>Décroissant</option>
-		<option value="asc">Croissant</option>
-	</select>
 
 
-
-	<img src="<?php bloginfo('template_directory') ?>/images/spinner.gif" alt="loader" class="loader"/>
-
-	<br/><br/>
-
-	<ul id="results"></ul>
-
+<section id="main" class="box-wrap antiscroll-wrap">
+			<div id="content" class="box">
+				<ul class="photos_list antiscroll-inner">
+					<li>
+						<a href="projet.html">
+							<article>
+								<div class="title">
+																								<span>01</span>
+									<h2><?php the_title(); ?></h2>
+								</div><!--END .title-->
+								<img alt="#" src="<?php echo get_field('picture') ?>" />
+							</article>
+						</a>
+					</li>
+					<li>
+						<a href="projet.html">
+							<article>
+								<div class="title">												<span>02</span>
+									<h2>Direction régionale des impôts</h2>
+								</div><!--END .title-->
+								<img alt="#" src="images/visuals/photo_thumb2.jpg" />
+							</article>
+						</a>
+					</li>
+					<li>
+						<a href="projet.html">
+							<article>
+								<div class="title">
+																								<span>03</span>
+									<h2>Direction régionale des impôts</h2>
+								</div><!--END .title-->
+								<img alt="#" src="images/visuals/photo_thumb3.jpg" />
+							</article>
+						</a>
+					</li>
+					<li>
+						<a href="projet.html">
+							<article>
+								<div class="title">
+																							<span>04</span>				
+									<h2>Direction régionale des impôts</h2>
+								</div><!--END .title-->
+								<img alt="#" src="images/visuals/photo_thumb4.jpg" />
+							</article>
+						</a>
+					</li>
+					<li>
+						<a href="projet.html">
+							<article>
+								<div class="title">												<span>05</span>
+									<h2>Direction régionale des impôts</h2>
+								</div><!--END .title-->
+								<img alt="#" src="images/visuals/photo_thumb5.jpg" />
+							</article>
+						</a>
+					</li>
+					<li>
+						<a href="projet.html">
+							<article>
+								<div class="title">
+									<span>06</span>
+									<h2>Direction régionale des impôts</h2>
+								</div><!--END .title-->
+								<img alt="#" src="images/visuals/photo_thumb6.jpg" />
+							</article>
+						</a>
+					</li>
+					<li>
+						<a href="projet.html">
+							<article>
+								<div class="title">
+									<span>07</span>
+									<h2>Direction régionale des impôts</h2>
+								</div><!--END .title-->
+								<img alt="#" src="images/visuals/photo_thumb1.jpg" />
+							</article>
+						</a>
+					</li>
+					<li>
+						<a href="projet.html">
+							<article>
+								<div class="title">
+									<span>08</span>
+									<h2>Direction régionale des impôts</h2>
+								</div><!--END .title-->
+								<img alt="#" src="images/visuals/photo_thumb2.jpg" />
+							</article>
+						</a>
+					</li>
+					<li>
+						<a href="projet.html">
+							<article>
+								<div class="title">
+									<span>09</span>
+									<h2>Direction régionale des impôts</h2>
+								</div><!--END .title-->
+								<img alt="#" src="images/visuals/photo_thumb3.jpg" />
+							</article>
+						</a>
+					</li>
+					<li>
+						<a href="projet.html">
+							<article>
+								<div class="title">
+									<span>10</span>
+									<h2>Direction régionale des impôts</h2>
+								</div><!--END .title-->
+								<img alt="#" src="images/visuals/photo_thumb4.jpg" />
+							</article>
+						</a>
+					</li>
+					<li>
+						<a href="projet.html">
+							<article>
+								<div class="title">
+									<span>11</span>
+									<h2>Direction régionale des impôts</h2>
+								</div><!--END .title-->
+								<img alt="#" src="images/visuals/photo_thumb5.jpg" />
+							</article>
+						</a>
+					</li>
+					<li>
+						<a href="projet.html">
+							<article>
+								<div class="title">
+									<span>12</span>
+									<h2>Direction régionale des impôts</h2>
+								</div><!--END .title-->
+								<img alt="#" src="images/visuals/photo_thumb6.jpg" />
+							</article>
+						</a>
+					</li>
+<li>
+						<a href="projet.html">
+							<article>
+								<div class="title">
+									<span>13</span>
+									<h2>Direction régionale des impôts</h2>
+								</div><!--END .title-->
+								<img alt="#" src="images/visuals/photo_thumb1.jpg" />
+							</article>
+						</a>
+					</li>
+					<li>
+						<a href="projet.html">
+							<article>
+								<div class="title">
+									<span>14</span>
+									<h2>Direction régionale des impôts</h2>
+								</div><!--END .title-->
+								<img alt="#" src="images/visuals/photo_thumb2.jpg" />
+							</article>
+						</a>
+					</li>
+					<li>
+						<a href="projet.html">
+							<article>
+								<div class="title">
+									<span>15</span>
+									<h2>Direction régionale des impôts</h2>
+								</div><!--END .title-->
+								<img alt="#" src="images/visuals/photo_thumb3.jpg" />
+							</article>
+						</a>
+					</li>
+				</ul>
+			</div><!--END #content-->
+		</section><!--END #main-->
 </div>
 
 <?php get_footer(); ?>
