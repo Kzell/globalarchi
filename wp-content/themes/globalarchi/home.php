d11433fd27b00bd84e7d14501544d71811ea4980
Loick <loick.ledigabel@gmail.com>
2012-12-20 02:13:39 +0100

    merge css

--- wp-content/themes/globalarchi/home.php
+++ wp-content/themes/globalarchi/home.php
@@ -22,7 +22,11 @@
 							<div class="description_projet">
 								<div class="inner">
 									<h2><?php the_title();?></h2>
-									<img  alt="#" src="<?php the_field('picture');?>"/>
+									<?php 
+										$image = get_field('picture');
+										$image = $image["sizes"]["large"];
+									?>
+									<img  alt="#" src="<?php echo $image; ?>"/>
 									<div id="content">
 										<ul class="description_list">
 											<li>
